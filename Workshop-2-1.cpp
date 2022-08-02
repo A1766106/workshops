f41ff38bb6051b9968a0fb526ac2b29e23130823
Max Bruce <maxbruce@student-10-201-05-210.wireless.adelaide.edu.au>
2022-08-02 17:14:21 +0930

    cheer

---
+++ Workshop-2-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+// Activity 1
+
+// 75 from base 10 to base 2 = 101001
+// 1100111011 = 443
+
+// Activity 2
+
+// 113 # 75 from base 10 to base 8
+// 1346742 from base 8 to base 10 is 380385
+
+// Activity 3
+
+// 4B # 75 from base 10 to base 16
+// 413674 # from base 16 to base 10
+
+// Activity 4
+
+// Stored as an array
+// Id make the base calculation a variable
+
+//Activity 5
+
+int transformer(int array[], int)
+{
+
+}
---
+++ Workshop-2-2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+// 111011101110
+
+//
